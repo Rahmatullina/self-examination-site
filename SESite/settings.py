b8d1179ee674ed7e3acf2a403656242760d8fd59
Emiliya <st054615@student.spbu.ru>
2019-09-18 23:01:20 +0300

    prepared for deploy

--- SESite/settings.py
+++ SESite/settings.py
@@ -20,12 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'g*3b4px@c^5-d!b93o71@o%-f31pbog37v$cv1_fz(lcmc9$zi'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = False
 
 ALLOWED_HOSTS = ['evening-lowlands-62772.herokuapp.com','localhost']
 
@@ -96,10 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -110,21 +104,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/result_form/residential_premises/' + dt.today().strftime('%Y/%m/')
 
-# EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-# SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ChangePassItBashkortostan@gmail.com'
-EMAIL_HOST_PASSWORD = 'Uk39FQ73'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 
 #for static files
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -136,14 +125,14 @@ import django_heroku
 django_heroku.settings(locals())
 
 # Heroku: Update database configuration from $DATABASE_URL.
-
 DATABASES = {}
+
 DATABASES['default'] = {
 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-'NAME': 'dejr93536c3haj',
-'USER' : 'xfmfyuwzkmvrlt',
-'PASSWORD' : '0ab41e08788c52462a39064b377857fba2e95e292b4ab7af3368800bdfe7883c',
-'HOST' : 'ec2-184-73-216-48.compute-1.amazonaws.com',
+'NAME': os.environ.get('DB_NAME'),
+'USER' : os.environ.get('DB_USER'),
+'PASSWORD' : os.environ.get('DB_PASSWORD'),
+'HOST' : os.environ.get('DB_HOST'),
 'PORT' : '5432',
 }
 AUTH_USER_MODEL = 'app.CustomUser'
